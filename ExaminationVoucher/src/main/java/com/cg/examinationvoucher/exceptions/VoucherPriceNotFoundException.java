2a1a0161c4b70351658724286bf32ba00c71807a
ashutoshlonikar <88879492+ashutoshlonikar@users.noreply.github.com>
2022-05-14 09:36:40 +0530

    Delete ExaminationVoucher directory

--- ExaminationVoucher/src/main/java/com/cg/examinationvoucher/exceptions/VoucherPriceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.cg.examinationvoucher.exceptions;
-@SuppressWarnings("serial")
-public class VoucherPriceNotFoundException extends RuntimeException{
-	public VoucherPriceNotFoundException(String message) {
-		super(message);
-	}
-
-}
